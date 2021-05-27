80f0b479df9754b6fd08ccd57b547b6f518843b2
Karl David <68324556+TheKarlDavid@users.noreply.github.com>
2021-05-27 20:13:29 +0800

    Taste Eat App

---
+++ app/src/main/java/ph/dlsu/s11/davidk/taste_eat/AccountActivity.java
@@ -0,0 +1,89 @@
+package ph.dlsu.s11.davidk.taste_eat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AccountActivity extends AppCompatActivity {
+
+    private TextView tv_email, tv_logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        init();
+
+        navbar();
+    }
+
+    private void init(){
+        tv_email = findViewById(R.id.tv_email);
+        tv_logout = findViewById(R.id.tv_logout);
+
+        SharedPreferences sp = getSharedPreferences("APP_USER", Context.MODE_PRIVATE);
+        String str_user_email = sp.getString("user", "default");
+
+        Log.d("TAGS", "user" + str_user_email);
+
+        tv_email.setText(str_user_email);
+
+        tv_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sp = getSharedPreferences("APP_USER", Context.MODE_PRIVATE);
+                SharedPreferences.Editor EDITOR = sp.edit();
+                EDITOR.clear();
+                EDITOR.apply();
+
+                Intent intent = new Intent(AccountActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                Log.d("TAGS", "Logout");
+            }
+        });
+
+    }
+
+    private void navbar(){
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setSelectedItemId(R.id.nav_account);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_menu_book:
+                        startActivity(new Intent(getApplicationContext(),
+                                HomeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.nav_bookmark:
+                        startActivity(new Intent(getApplicationContext(),
+                                SavedActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_favorites:
+                        startActivity(new Intent(getApplicationContext(),
+                                LikedActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_account:
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
